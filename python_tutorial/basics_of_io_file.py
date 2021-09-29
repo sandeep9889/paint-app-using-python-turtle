2c79ae425a8cc0cade07e61da3d2598797fa10d4
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-30 00:35:11 +0530

    file io

--- python_tutorial/basics_of_io_file.py
+++ python_tutorial/basics_of_io_file.py
@@ -7,13 +7,25 @@
 "a"= for appending a file
 """
 
-f =open("sandeep.txt", "rt")
-print(f.readline()) #readline function used to read individual line 
-print(f.readlines())#it can all lines in list and from above read line we have only remaining two line
-
-
-
-
-
+# f =open("sandeep.txt", "r")
+# print(f.readline()) #readline function used to read individual line /
+# print(f.readlines())#it can all lines in list and from above read line we have only remaining two line
 # content=f.read() # read to read datav
 # print(content)
+# f.close()
+
+
+# for write
+# f = open("sandeep.txt1","w") #here is no sandeep1.txt file but it will build it self
+# f.write("hello sandeep kaise ho")
+
+
+
+#for read and write
+f = open("sandeep.txt1","r+")
+san=f.read() #for read
+print(san) 
+f.write("college konsa h aapka aur dewas m kha rahete ho") #it can be write in the sandeep.txt1 file
+
+
+
---
+++ python_tutorial/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+# f = open("sandeep.txt1","w") #here is no sandeep1.txt file but it will build it self
+# f.write("hello sandeep kaise ho")
