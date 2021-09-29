d7369d734b607118955d4ab580c40aee54ae8f94
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-30 01:04:57 +0530

    seek and tell

--- python_tutorial/basics_of_io_file.py
+++ python_tutorial/basics_of_io_file.py
@@ -22,10 +22,20 @@
 
 
 #for read and write
-f = open("sandeep.txt1","r+")
-san=f.read() #for read
-print(san) 
-f.write("college konsa h aapka aur dewas m kha rahete ho") #it can be write in the sandeep.txt1 file
+# f = open("sandeep.txt1","r+")
+# san=f.read() #for read
+# print(san) 
+# f.write("college konsa h aapka aur dewas m kha rahete ho") #it can be write in the sandeep.txt1 file
+
+
+#to know and reset about where is file pointer location
+f = open("sandeep.txt1", "r")
+f.readline()
+tld=f.tell() #it will give the information wherer is file pointer lie 
+reset =  f.seek() #it will reset file pointer like  f.seek(10) so here it will start from 10 index
+
+f.read()
+
 
 
 
