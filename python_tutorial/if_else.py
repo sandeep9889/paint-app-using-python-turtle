3984fd6fa1974e05bdf0414affbf33657b12fc71
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-29 21:48:22 +0530

    short hand if else

---
+++ python_tutorial/if_else.py
@@ -0,0 +1,6 @@
+#short hand if else
+a = int(input("enter a\n"))
+b = int(input("enter b\n"))
+print("a b se bda h") if(a>b) else print("b a se bda h bhai")
+#here it is called short handed because it is in one line code
+#we can write here firstly print statement and then we have to write conditional if statement
