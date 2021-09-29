c526cd8aa94f3bbfc0250f0a2a3730f58e26d164
sandeep <sandeepchauhan9630228313@gmail.com>
2021-09-29 23:58:16 +0530

    except and try

--- python_tutorial/try__except.py
+++ python_tutorial/try__except.py
@@ -3,9 +3,9 @@ print("0enter no 1")
 num1= (input())
 print("enter no 2")
 num2=(input())
-try:
+try:    #here  try will work on it if is possible then it will run otherwise it will work to another accept it does stop our progrramme
     print("teh sum of number is",int(num1)+int(num2))
-except Exception as e:
+except Exception as e: #here if num1=45 and num2=hj then try was not working then try except as e will be on 
     print(e)
 
 
