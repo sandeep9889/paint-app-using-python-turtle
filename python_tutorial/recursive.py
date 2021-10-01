c56baf6607075e88a9a25b4b84fb11c773fec92b
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-01 11:04:18 +0530

    health_management_sys

---
+++ python_tutorial/recursive.py
@@ -0,0 +1,28 @@
+#here  we have to build recursive funvction
+#n*(n-1)!
+def recursive():
+    if n==1:
+        return 1
+    else:
+        return n * recursive(n-1)
+        
+
+num = int(input("enter a number :"))
+print("by recursion",recursive(num))
+
+
+
+#for febonacci series
+# 0 1 1 2 3 
+# def fibonacci(n):
+#     if n==0:
+#         return 0 
+
+#     elif n==1:
+#         return 1
+
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)       
+# num = int(input("enter no of fibonacci series:"))
+# print(fibonacci(num))
+    
