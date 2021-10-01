29c99e087bec27c36045127d23b906651e733cb6
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-01 12:59:14 +0530

    fstring

---
+++ python_tutorial/f_string.py
@@ -0,0 +1,6 @@
+#f string is to save time on adding strings etc
+import math
+me = "sandeep"
+marks = 80
+a = f"this is  {me} {marks} math.cos(65)" #here f is used as fstring
+print(a)  
--- python_tutorial/lambda_function.py
+++ python_tutorial/lambda_function.py
@@ -1,5 +1,6 @@
 #lambda function is one line function
 
+
 def minus(a,b):
     return a-b
 
@@ -7,5 +8,5 @@ def minus(a,b):
 minus1 = lambda a,b:a-b
 
 # print("minus function:",minus)
-# minus1(9,4)
+
 print(minus1(9,4))
