c571eb9ac50fa4faa2058a3609c511978b3bc8ba
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-01 18:04:07 +0530

    enumenator

---
+++ python_tutorial/enumerate.py
@@ -0,0 +1,17 @@
+#enumerate can make life easy
+
+
+
+#it is simple process
+l1= ["bhindi", "aloo", "chowmein", "kandaa"]
+# i =1
+# for item in l1:
+#     if i%2 is not 0:
+#         print(item)
+#     i+=1    
+
+#if you want to simple
+for index,item in enumerate(l1): #but here index is starting from 0 
+    if index%2==0:
+        print(item)
+
