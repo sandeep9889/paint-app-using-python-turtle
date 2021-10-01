544b9489e53174372e002d8ea60712269bd84951
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-01 10:19:05 +0530

    health_management_sys

--- python_tutorial/health_management_sys/health_management_sys.py
+++ python_tutorial/health_management_sys/health_management_sys.py
@@ -65,7 +65,7 @@ def retrive(k):
     if k ==1:
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c ==1):
-            value=input("type here/n")
+            
             with open ("sandeep_ex.txt")as op:
                 for i in op:
                     print(i, end="")
@@ -73,8 +73,8 @@ def retrive(k):
             
 
         elif(c ==2):
-            value1 = input("type here/n")
-            with open ("sandeep_ex.txt")as op:
+            
+            with open ("sandeep_fd.txt")as op:
                 for i in op:
                     print(i, end="")
 
@@ -82,7 +82,7 @@ def retrive(k):
     if k ==2:
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c ==1):
-            value=input("type here/n")
+           
             with open ("hemant_ex.txt")as op:
                 for i in op:
                     print(i, end="")
@@ -90,8 +90,8 @@ def retrive(k):
             print("succesfully  written")
 
         elif(c ==2):
-            value1 = input("type here/n")
-            with open ("hemant_ex.txt")as op:
+           
+            with open ("hemant_fd.txt")as op:
                 for i in op:
                     print(i, end="")
 
@@ -102,20 +102,18 @@ def retrive(k):
     if k ==3:
         c=int(input("enter 1 for excersise and 2 for food"))
         if(c ==1):
-            value=input("type here/n")
+            
             with open ("rahul_ex.txt")as op:
                 for i in op:
                     print(i, end="")
 
-            print("succesfully  written")
+            
 
         elif(c ==2):
-            value1 = input("type here/n")
-            with open("rahul_fd.txt","a") as op:
-                op.write(str[str(getdate())]+":"+value1)
-
-            print("succesfully writen")   
-
+            
+           with open ("rahul_fd.txt")as op:
+                for i in op:
+                    print(i, end="")
 
 
 print("Welcome to health management system")
@@ -125,7 +123,7 @@ if(inp == 1):
     take(people)
 elif(inp == 0):
     people_retrive = int(input("for sandeep:1 for hemant 2 for rahuk :3"))
-    take(people_retrive)
+    retrive(people_retrive)
 
 else:
     print("give proper input")    
