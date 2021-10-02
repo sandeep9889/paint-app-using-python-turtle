eb5986b18e98cd2139a616cf7c2e5b8653dc8113
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-02 13:40:44 +0530

    classes_object_and instance variabkle

---
+++ python_tutorial/oops/oops1.py
@@ -0,0 +1,15 @@
+#this is our first oops programming code 
+class student:       #here  we initialize a class
+    pass                     #pass means noting to do
+
+
+sandeep = student()  #this is an object
+kumar  =student()
+sandeep.name = "sandy"   #this is instance variable that has to build in classs
+sandeep.clg = "sati"
+sandeep.branch = "aiads"
+kumar.name = "shiv"
+kumar.clg  = "baktara sarkari"
+kumar.branch = "cse"
+
+print(sandeep.name,sandeep.clg,kumar.clg)
