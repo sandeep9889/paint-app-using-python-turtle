caafbc0b19775411cd41ff3065cc3b0e8df5d84d
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-02 12:29:03 +0530

    starting_oops

--- python_tutorial/oops/oops_basic.py
+++ python_tutorial/oops/oops_basic.py
@@ -5,3 +5,15 @@
 
 """classes jo hoti h vo just like a build in form 
  and object jo  hota h vo us form ko bharne k liye"""
+
+
+
+
+
+ #like one mmore example by flim industry
+"""
+if you build a movie 
+zero-  stk baua singh aur jo h ki bauna  === templates (clases)
+jo movie bn jayegi  == (object)
+pr agr movie hritik bnayeka to  us class ka dusra instance matlv ki dusra object
+"""
