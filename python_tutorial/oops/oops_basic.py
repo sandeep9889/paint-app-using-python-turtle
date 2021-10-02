b2a22ada1532a11b290badf0a0cd6d2e9e92f3c5
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-02 12:22:35 +0530

    starting_oops

---
+++ python_tutorial/oops/oops_basic.py
@@ -0,0 +1,7 @@
+#we have learn here about object oriented programming
+# classes = template {like a buildin letter we have to just filling details that known as template as classses}
+# object - instance of class {vo pura khat}
+# DRY =  do not repeat principle 
+
+"""classes jo hoti h vo just like a build in form 
+ and object jo  hota h vo us form ko bharne k liye"""
