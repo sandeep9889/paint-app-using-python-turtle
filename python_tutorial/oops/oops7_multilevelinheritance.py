52c293daf223aac71cad0e167403db745d743026
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-02 19:38:25 +0530

    multilevel inheritence

---
+++ python_tutorial/oops/oops7_multilevelinheritance.py
@@ -0,0 +1,18 @@
+class electronicdevice:
+    time ="10hr"
+    memory = "1tb"
+
+class pocketdevice(electronicdevice):
+    size =150
+    charger = "65wat"
+    time = "5hr"
+class phone(pocketdevice):
+    price = 15000
+    ram ="8gb"
+
+
+lenevo = electronicdevice()
+tab = pocketdevice()
+jio= phone()
+
+print(phone.time)
