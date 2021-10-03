c678b2fc875376de27fe7790b020312d379d2af1
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-03 14:28:48 +0530

    library project

--- python_tutorial/oops/project_on_oops_library.py
+++ python_tutorial/oops/project_on_oops_library.py
@@ -15,7 +15,23 @@ class library:
     def __init__(self,list_of_books,libholder):
         self.list_of_books = list_of_books
         self.librarayholder = libholder
+    
+    @property
+    def display(self):
+        print(f"librarian {self.librarayholder}  librarian having books {self.list_of_books}")
+
+    @display.setter
+    def display(Self,string):
+        return Self.list_of_books + string.split[","]
+        
 
 
-sandeep = library(["chetan bhagat","harryporter","richdad poordad","time management","attitude"], "sandeeplibrary")   
-print(sandeep.list_of_books) 
+
+if __name__ =="__main__":
+
+    sandeep = library(["chetan bhagat","harryporter","richdad poordad","time management","attitude"], "sandeep library") 
+    sandeep.display = "araybhata,ramajun,elon musk ,the fairy tails"
+    print(sandeep.display)
+    
+    
+
