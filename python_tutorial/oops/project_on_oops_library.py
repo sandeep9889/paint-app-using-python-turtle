7dca3c217711a2ecd393fc7f139442ade4683bfa
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-04 10:57:11 +0530

    library project

--- python_tutorial/oops/project_on_oops_library.py
+++ python_tutorial/oops/project_on_oops_library.py
@@ -22,7 +22,7 @@ class library:
 
         
     def lendin(self,user,book):
-        if book not in self.lendict.key:
+        if book not in self.lendict:
             self.lendict.update({book:user})
             print("lender book data base has been updated . you can take the book now")
         else:
@@ -30,11 +30,11 @@ class library:
         
 
     def returnbook(self,user,book):
-        self.list_of_books.remove(book)
+        self.lendict.pop(book)
         print(f"book {book} has been return by {user}")
 
     def addbook(self,book):
-        self.list_of_book.append(book)
+        self.list_of_books.append(book)
         print(f"book has been added to book_list")
 
     @property
@@ -60,17 +60,40 @@ if __name__ =="__main__":
         print("3 . add a book")
         print("4 . return a books")
         user_choice = int(input())
+        if user_choice not in [1,2,3,4]:
+            continue
+        else:
+            user_choice = int(input())
 
         if user_choice ==1:
             sandeep.display
         elif user_choice ==2:
-            sandeep.lendict()
+            book = input(f"enter the name of the book from given books {sandeep.list_of_books}")
+            user =input("enter your name")
+            sandeep.lendin(user,book)
 
         elif user_choice ==3:
-            sandeep.addbook()
+            book = input("enter the name of the book you wwant to add")
+
+            sandeep.addbook(book)
 
         elif user_choice ==4:
-            sandeep.returnbook()            
+            book = input("enter the name of the book you wwant to return")
+            sandeep.returnbook(book) 
+
+        else:
+            print("not a valid option")
+
+        print("press q for quit and c for continew")
+        user_choice2=""
+        while(user_choice2!="c" and user_choice2 !="q"):
+            user_choice2 = input()
+            if user_choice2=="q":
+                exit()
+            elif user_choice2 == "c":
+                continue  
+
+
         
 
 
