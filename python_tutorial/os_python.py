86aa08180ea0fc02e4eb186e0f12d18c13a6668d
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-07 15:44:39 +0530

    os

--- python_tutorial/os_python.py
+++ python_tutorial/os_python.py
@@ -1,3 +1,11 @@
 # here os mewans operating system used as 
 import os
-print(dir(os))
+# print(dir(os)) #here dir means it can show dir  func to do
+print(os.getcwd()) #getcwd current working directory 
+#os.chdir("c://") #chdir change directory
+print(os.getcwd())
+print(os.listdir()) #to return a list  and show it how many files in here
+# os.mkdir("this") #to making folder 
+# os.makedirs("that/this")  #it can make folder to folder 
+# print(os.environ.get("path")) #here  you can get enviornment
+print(os.path.exists("c://"))
