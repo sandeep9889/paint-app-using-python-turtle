9a077f70ccef3fc5691632d1ab15b7aaa24e8f1e
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-07 22:06:22 +0530

    json

--- python_tutorial/json_python.py
+++ python_tutorial/json_python.py
@@ -1,3 +1,15 @@
 #javascript object notation
 # json used to send aur export data from web
-import json as js
+import json 
+data = '{"varr1":"sandeep", "varr2":"jatin"}'
+parsed =json.loads(data)
+print(parsed["varr1"])
+print(data)
+data2 = {
+    "chennal_name": "code with harry",
+    "cars": ['bmw','audi-a8','ferrari'],
+    "fridge":('roti',540),
+    "isbad": False
+}
+jscomb = json.dumps(data2)
+print(jscomb)
