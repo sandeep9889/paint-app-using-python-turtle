f03781bd98944600353a7bcf57f7e956299c261e
sandeep <sandeepchauhan9630228313@gmail.com>
2021-10-09 16:45:25 +0530

    pickle

---
+++ python_tutorial/regex_tut.py
@@ -0,0 +1,40 @@
+#meta chracter
+
+"""[]	A set of characters	"[a-m]"	
+\	Signals a special sequence (can also be used to escape special characters)	"\d"	
+.	Any character (except newline character)	"he..o"	
+^	Starts with	"^hello"	
+$	Ends with	"world$"	
+*	Zero or more occurrences	"aix*"	
++	One or more occurrences	"aix+"	
+{}	Exactly the specified number of occurrences	"al{2}"	
+|	Either or	"falls|stays"	
+()	Capture and group"""
+
+
+
+
+
+#sequence character
+"""\A	Returns a match if the specified characters are at the beginning of the string	"\AThe"	
+\b	Returns a match where the specified characters are at the beginning or at the end of a word
+(the "r" in the beginning is making sure that the string is being treated as a "raw string")	r"\bain"
+r"ain\b"	
+\B	Returns a match where the specified characters are present, but NOT at the beginning (or at the end) of a word
+(the "r" in the beginning is making sure that the string is being treated as a "raw string")	r"\Bain"
+r"ain\B"	
+\d	Returns a match where the string contains digits (numbers from 0-9)	"\d"	
+\D	Returns a match where the string DOES NOT contain digits	"\D"	
+\s	Returns a match where the string contains a white space character	"\s"	
+\S	Returns a match where the string DOES NOT contain a white space character	"\S"	
+\w	Returns a match where the string contains any word characters (characters from a to Z, digits from 0-9, and the underscore _ character)	"\w"	
+\W	Returns a match where the string DOES NOT contain any word characters	"\W"	
+\Z	Returns a match if the specified characters are at the end of the string"""
+
+
+import re
+mystr = ("hello dosto mera number h +919630228313")
+patt=re.compile(r'.+91\d{10}')
+matches=patt.finditer(mystr)
+for match in matches:
+    print(match)
